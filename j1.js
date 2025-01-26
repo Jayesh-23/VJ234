948e4914c53dda16e71d807e4cfc66b766819054
jayesh kathale <124416855+Jayesh-23@users.noreply.github.com>
2025-01-26 16:21:44 +0530

    js

---
+++ j1.js
@@ -0,0 +1,9 @@
+// Function to show the gallery
+function showGallery() {
+    document.getElementById('galleryContainer').style.display = 'block';
+}
+
+// Function to hide the gallery
+function hideGallery() {
+    document.getElementById('galleryContainer').style.display = 'none';
+}
